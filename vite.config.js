e35b1ab68c6c48ed27dd4d7ef5edf8b2d9e438ea
Nirman Malaviya <nirmanmalaviya529@gmail.com>
2024-08-04 13:19:05 -0700

    portfolio

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: { outDir: 'build' },
   plugins: [react()],
+  base: "/Nirman529.github.io/"
 });
